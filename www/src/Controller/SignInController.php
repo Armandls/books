eba445bd4dfeac15819214402c287286969f6e2a
armand <armand.lopez@students.salle.url.edu>
2024-04-20 19:37:33 +0200

    sign-in vista hecha

--- www/src/Controller/SignInController.php
+++ www/src/Controller/SignInController.php
@@ -4,6 +4,7 @@ namespace Project\Bookworm\Controller;
 
 use Project\Bookworm\Model\UserRepository;
 use Slim\Flash\Messages;
+use Slim\Routing\RouteContext;
 use Slim\Views\Twig;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -30,13 +31,13 @@ final class SignInController
     {
         //$messages = $this->flash->getMessages();
 
-        //$routeParser = RouteContext::fromRequest($request)->getRouteParser();
+        $routeParser = RouteContext::fromRequest($request)->getRouteParser();
 
         return $this->twig->render($response, 'sign-in.twig', []);
     }
 
     // Método que maneja el envío del formulario -> Validación de datos
-    /*public function handleFormSubmission(Request $request, Response $response): Response
+    public function handleFormSubmission(Request $request, Response $response): Response
     {
         $data = $request->getParsedBody();
 
@@ -55,43 +56,29 @@ final class SignInController
             $_SESSION['email'] = $data['email'];
             return $response->withHeader('Location', '/')->withStatus(302);
         }
-    }*/
+    }
 
-    /*private function validate(array $data, userRepository $userRepository): array
+    /*
+    private function validate(array $data, userRepository $userRepository): array
     {
         $errors = [];
 
         // Errores email
-        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL) || empty($data['email'])) {
+        if (empty($data['email'])) {
             $errors['email'] = 'The email address is not valid.';
         }
         else {
-            if (!strpos($data['email'], '@salle.url.edu')){
-                $errors['email'] = 'Only emails from the domain @salle.url.edu are accepted.';
+            if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors['email'] = 'The email address is not valid.';
             }
             else {
                 $user = $userRepository->findByEmail($data['email']);
-                if ($user == null) {
-                    $errors['email'] = 'User with this email address does not exist.';
+                $storedPassword = $user->password();
+
+                if ($data['password'] != $storedPassword) {
+                    $errors['password'] = 'Your email and/or password are incorrect.';
                 }
-                else {
-                    $storedPasswordHash = $user->password();
-                    if ($data['password'] != $storedPasswordHash) {
-                        $errors['password'] = 'Your email and/or password are incorrect.';
-                    }
-                }
-            }
-        }
 
-
-        // Errores password
-
-        if (strlen($data['password']) < 7) {
-            $errors['password'] = 'The password must contain at least 7 characters.';
-        }
-        else {
-            if (!preg_match('/[A-Z]/', $data['password']) || !preg_match('/[a-z]/', $data['password']) || !preg_match('/[0-9]/', $data['password'])) {
-                $errors['password'] = 'The password must contain both upper and lower case letters and numbers.';
             }
         }
 

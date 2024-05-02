62f9264769bfe325e87f17d7c834407ed8e5536f
armand <armand.lopez@students.salle.url.edu>
2024-05-02 20:31:39 +0200

    quitar los dobles setnames en routing de profile

--- www/config/routing.php
+++ www/config/routing.php
@@ -28,9 +28,9 @@ $app->get('/sign-in', SignInController::class . ':showForm')->setName('get-sign-
 $app->post('/sign-in', SignInController::class . ':handleFormSubmission')->setName('login-form');
 
 // 6- Cuando me llegue una petición GET a la ruta /profile, se ejecutarán los métodos showProfile de la clase UserProfile
-$app->get('/profile', UserProfile::class . ':showProfile')->setName('profile')->setName('show-profile');
+$app->get('/profile', UserProfile::class . ':showProfile')->setName('show-profile');
 // 7- Cuando me llegue una petición POST a la ruta /profile, se ejecutarán los métodos editProfile de la clase UserProfile
-$app->post('/profile', UserProfile::class . ':editProfile')->setName('profile')->setName('edit-profile');
+$app->post('/profile', UserProfile::class . ':editProfile')->setName('edit-profile');
 
 // 8- Cuando me llegue una petición GET a la ruta /catalogue, se ejecutará el método apply de la clase CatalogueController
 $app->get('/catalogue', CatalogueController::class . ':showCatalogue')->setName('catalogue');
